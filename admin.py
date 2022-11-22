f4bb2120888b7f20900e8f0727eaef302c528a97
Manjujohn3 <manjujohn301@gmail.com>
2022-11-22 19:30:45 +0530

    kseb menu

---
+++ admin.py
@@ -0,0 +1,43 @@
+while True:
+    
+    print("Select an option")
+    print("1 Add consumer")
+    print("2 search consumer")
+    print("3 delete consumer")
+    print("4 update consumer")
+    print("5 view all consumer")
+    print("6 Generate bill")
+    print("7 view bill")
+    print("8 Top 2 high bill")
+    print("9 exit")
+
+    choice = int(input("Enter an option: "))
+    if(choice==1):
+        print("add consumer selected")
+
+    elif(choice==2):
+         print("search consumer selected")
+        
+    elif(choice==3):
+        print("delete consumer selected")
+    
+    
+    elif(choice==4):
+        print("update consumer selected")
+
+    elif(choice==5):
+        print("view all consumer selected")
+
+
+    elif(choice==6):
+        print("generate bill selected")
+    
+
+    elif(choice==7):
+        print("view billselected")
+        
+    elif(choice==8):
+        print('Top 2 high bill')
+        
+    elif(choice==9):
+        break
